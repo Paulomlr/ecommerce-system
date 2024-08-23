5f8f52cf0ca5372b594e4ba7ca0de7634b6fb9c0
Paulo Cesar <paulocesarpinheirodasilva0@gmail.com>
2024-08-23 15:38:09 -0300

    feat: add user entity and UserRole enumeration

---
+++ src/main/java/com/paulomlr/ecommerceSystem/domain/enums/UserRole.java
@@ -0,0 +1,13 @@
+package com.paulomlr.ecommerceSystem.domain.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum UserRole {
+    ADMIN("admin"),
+    USER("user");
+
+    private final String role;
+
+    UserRole(String role) {this.role = role;}
+}
